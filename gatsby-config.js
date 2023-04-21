b43d734767f28baafbdac0a41d5c05ae9255beb9
Nai Lewis <babygirl@Naiyahs-MacBook-Pro-2.local>
2023-04-21 15:20:21 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Blended`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
