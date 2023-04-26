b7dd8e97e1fe41b4f4ab2560c18adbcb56a35b36
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2023-04-26 17:15:27 -0400

    updated index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,37 @@
 import React from "react"
 //import styled from "@emotion/styled"
 //import { css } from "@emotion/react"
-import Navbar from "../components/navigation"
-//import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  //padding: 92,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 84,
-  maxWidth: 1020,
-}
-
-//"just made Gatsby Site"
-const headingAccentStyles = {
-  color: "#663399",
-}
-
+import App from './App';
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <Navbar></Navbar>
-      <h1 style={headingStyles}>
-        Welcome to Blended!
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-    </main>
+    <App/>
   )
 }
 
 export default IndexPage
 
 export const Head = () => <title>Home Page</title>
+
+
+// <h1 style={headingStyles}>
+// Welcome to Blended!
+// <br />
+// <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+// </h1>
+
+// const pageStyles = {
+//   color: "#232129",
+//   //padding: 92,
+//   fontFamily: "-apple-system, Roboto, sans-serif, serif",
+// }
+// const headingStyles = {
+//   marginTop: 0,
+//   marginBottom: 84,
+//   maxWidth: 1020,
+// }
+// const headingAccentStyles = {
+//   color: "#663399",
+// }
+
+// //"just made Gatsby Site"
+//import { Link } from "gatsby"
