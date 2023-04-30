d8c2e40e5426b6a0c3b457f9356e6dcb55f2de8e
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2023-04-30 13:59:32 -0400

    Added on submit listener

--- src/components/NavBarComponent/AddNodeBar.js
+++ src/components/NavBarComponent/AddNodeBar.js
@@ -3,16 +3,22 @@ import "./Navbar.css"
 // import { SideNav } from './SideNav';
 
 
-const AddNodeBar = ({ onGoBackClick }) => {
+const AddNodeBar = ({ onGoBackClick , onSubmit }) => {
     return (
     
         <nav id="add-node-nav" class="hidden">
             <button id="go-back" onClick={onGoBackClick} >Go Back</button>
-            <form id="create-node-form">
+            <form id="create-node-form" onSubmit={onSubmit}>
                 <label>
                     Name:
                     <input type="text" id="name" required/>
                 </label>
+
+                <label>
+                    DOB:
+                    <input type="text" id="dob" required/>
+                </label>
+
                 <label>
                     <input type="radio" name="relation" value="sibling" required/> Sibling
                 </label>
@@ -31,8 +37,6 @@ const AddNodeBar = ({ onGoBackClick }) => {
         </nav>
     )
   }
-
-
 
 
 export default AddNodeBar
