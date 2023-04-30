25e6647c497a1bdc0ee440d8b5327337b1def7bf
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2023-04-30 14:00:50 -0400

    adjusted nav bar

--- src/components/NavBarComponent/Navbar.js
+++ src/components/NavBarComponent/Navbar.js
@@ -3,10 +3,10 @@ import "./Navbar.css"
 
 const MainNavBar = ({ onAddNodeClick }) => {
     return(
-        <> 
+        <>
         <nav id="main-nav">
-        <h1>Blended</h1>
-        <button id="add-node" onClick={onAddNodeClick}>Add Node</button>
+        <h1> Blended</h1>
+        <button id="add-node" onClick={onAddNodeClick}>Add Person</button>
         </nav>
         </>
     )
