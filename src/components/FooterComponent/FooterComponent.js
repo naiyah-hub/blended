0165980ac75c54129e957b655d772ec9082e6855
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2023-05-11 07:03:47 -0400

    fixed class -> className

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -1,36 +1,30 @@
 import * as React from "react";
 import "./FooterComponent.css"
 
-function Footer ({onZoomIn, onZoomOut, onEnterFullscreen}){
+function Footer({onZoomIn, onZoomOut, onEnterFullscreen}) {
     return(
       <footer>
         <link
           href= 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' 
           rel= "stylesheet"
         /> 
-        <div class="footer-icon" onClick={onZoomIn}>
-          <div class="sym">
-            <i class='bx bx-zoom-in graphic'></i>
+        <button className="footer-icon" onClick={onZoomIn}>
+          <div className="sym">
+            <i className='bx bx-zoom-in graphic'></i>
           </div>
-        </div>
-        <div class="footer-icon" onClick={onZoomOut}>
-          <div class="sym">
-            <i class='bx bx-zoom-out graphic'></i>
+        </button>
+        <button className="footer-icon" onClick={onZoomOut}>
+          <div className="sym">
+            <i className='bx bx-zoom-out graphic'></i>
           </div>
-        </div>
-        <div class="footer-icon" onClick={onEnterFullscreen}>
-          <div class="sym">
-            <i class='bx bx-fullscreen graphic'></i>
+        </button>
+        <button className="footer-icon" onClick={onEnterFullscreen}>
+          <div className="sym">
+            <i className='bx bx-fullscreen graphic'></i>
           </div>
-        </div>
-        <div class="footer-icon">
-          <div class="sym">
-            <i class='bx bx-exit-fullscreen graphic-last'></i>
-          </div>
-        </div>
+        </button>
       </footer>
     )
-  }
-  
-  export default Footer
-  
+}
+
+export default Footer
--- src/components/NavBarComponent/Nav.js
+++ src/components/NavBarComponent/Nav.js
@@ -1,9 +1,16 @@
 import * as React from "react";
 import html2canvas from 'html2canvas';
 import { saveAs } from 'file-saver';
+import { useState } from "react";
+
 import "./Nav.css"
 
 const Nav = ({ onAddNodeClick }) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    
+    const handleLogoClick = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+      };
     
     const handleSaveClick = () => {
         const input = document.querySelector('.family-tree-container');
@@ -21,53 +28,52 @@ const Nav = ({ onAddNodeClick }) => {
             rel= "stylesheet"
             />   
 
-            <div class="sidebar">
+            <div className="sidebar">
                 
-                <div class="logo">
-                    <i class='bx bx-menu menu-icon'></i>
-                    <span class="logo-name"></span>
+                <div className="logo" onClick={handleLogoClick}>
+                    <i className='bx bx-menu menu-icon'></i>
+                    <span className="logo-name"></span>
                 </div>
-
-
-                <div class="sidebar-content">
-                    <ul class="lists">
-                        <li class="list">
-                            <a href="#" class="nav-link">
-                                <i class="bx bx-home-alt icon"></i>
-                                <span class="link">Dashboard</span>
+                {/* <div className={`sidebar ${isSidebarOpen ? '' : 'closed'}`}></div> */}
+                <div className="sidebar-content">
+                    <ul className="lists">
+                        <li className="list">
+                            <a href="#!" className="nav-link">
+                                <i className="bx bx-home-alt icon"></i>
+                                <span className="link">Dashboard</span>
                             </a>
                         </li>
-                        <li class="list">
-                            <a class="nav-link" onClick={onAddNodeClick}>
-                                <i class='bx bxs-tree icon'></i>
-                                <span class="link">Family Tree</span>
+                        <li className="list">
+                            <a className="nav-link" onClick={onAddNodeClick}>
+                                <i className='bx bxs-tree icon'></i>
+                                <span className="link">Family Tree</span>
                             </a>
                         </li>
-                        <li class="list">
-                            <a href="#" class="nav-link">
-                                <i class='bx bxs-share icon' ></i>
-                                <span class="link">Share</span>
+                        <li className="list">
+                            <a href="#!" className="nav-link">
+                                <i className='bx bxs-share icon' ></i>
+                                <span className="link">Share</span>
                             </a>
                         </li>
-                        <li class="list">
-                            <a href="#" class="nav-link" onClick={handleSaveClick}>
-                            <i class='bx bxs-down-arrow-circle icon'></i>
-                            <span class="link">Save</span>
+                        <li className="list">
+                            <a href="#!" className="nav-link" onClick={handleSaveClick}>
+                            <i className='bx bxs-down-arrow-circle icon'></i>
+                            <span className="link">Save</span>
                             </a>
                         </li>
-                        <li class="list">
-                            <a href="#" class="nav-link">
-                                <i class='bx bx-folder-open icon' ></i>
-                                <span class="link">Files</span>
+                        <li className="list">
+                            <a href="#!" className="nav-link">
+                                <i className='bx bx-folder-open icon' ></i>
+                                <span className="link">Files</span>
                             </a>
                         </li>
 
                         </ul>
-                        <div class="bottom-content">
-                             <li class="list">
-                                 <a href="#" class="nav-link">
-                                    <i class='bx bx-log-out icon' ></i>
-                                    <span class="link">Logout</span>
+                        <div className="bottom-content">
+                             <li className="list">
+                                 <a href="#!" className="nav-link">
+                                    <i className='bx bx-log-out icon' ></i>
+                                    <span className="link">Logout</span>
                                 </a>
                             </li>
                         </div>
