d3b89d89ea4cd7f2b40acdfb9e26a41401100449
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2023-05-11 04:20:37 -0400

    Implmeneted a functions for the footer zoom in/out Fullscreen

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -1,35 +1,36 @@
 import * as React from "react";
 import "./FooterComponent.css"
 
-function Footer (){
+function Footer ({onZoomIn, onZoomOut, onEnterFullscreen}){
     return(
-        <footer>
-            <link
-            href= 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' 
-            rel= "stylesheet"
-            /> 
-            <div class="footer-icon">
-                <div class="sym">
-                   <i class='bx bx-zoom-in graphic' ></i>
-                </div>
-            </div>
-            <div class="footer-icon">
-                <div class="sym">
-                    <i class='bx bx-zoom-out graphic' ></i>
-                </div>
-            </div>
-            <div class="footer-icon">
-                <div class="sym">
-                    <i class='bx bx-fullscreen graphic' ></i>
-                </div>
-            </div>
-            <div class="footer-icon">
-                <div class="sym">
-                    <i class='bx bx-exit-fullscreen graphic-last' ></i>
-                </div>
-            </div>
-    </footer>
+      <footer>
+        <link
+          href= 'https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' 
+          rel= "stylesheet"
+        /> 
+        <div class="footer-icon" onClick={onZoomIn}>
+          <div class="sym">
+            <i class='bx bx-zoom-in graphic'></i>
+          </div>
+        </div>
+        <div class="footer-icon" onClick={onZoomOut}>
+          <div class="sym">
+            <i class='bx bx-zoom-out graphic'></i>
+          </div>
+        </div>
+        <div class="footer-icon" onClick={onEnterFullscreen}>
+          <div class="sym">
+            <i class='bx bx-fullscreen graphic'></i>
+          </div>
+        </div>
+        <div class="footer-icon">
+          <div class="sym">
+            <i class='bx bx-exit-fullscreen graphic-last'></i>
+          </div>
+        </div>
+      </footer>
     )
-}
-
-export default Footer
+  }
+  
+  export default Footer
+  
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Nav from "../components/NavBarComponent/Nav"
 import Footer from "../components/FooterComponent/FooterComponent"
 import AddNodeBar from "../components/NavBarComponent/AddNodeBar"
 import FamilyTree  from '../components/TreeComponent/familyTree';
 import members, { addChild } from '../components/TreeComponent/family';
+import Header from "../components/HeaderComponent/Header";
+
+
 
 const App = () => {
+
+  const contentContainerRef = useRef(null);
+
   const [showAddNodeBar, setShowAddNodeBar] = useState(false);
 
   const handleAddNodeClick = () => {
@@ -65,30 +71,66 @@ const App = () => {
     // Reset the form
     event.target.reset();
   };
+
+  const handleZoomIn = () => {
+    if (contentContainerRef.current) {
+      contentContainerRef.current.style.transform = 'translate(-50%, -50%) scale(1.1)';
+    }
+  };
+  
+  const handleZoomOut = () => {
+    if (contentContainerRef.current) {
+      contentContainerRef.current.style.transform = 'translate(-50%, -50%) scale(0.9)';
+    }
+  };
+  
+  const handleEnterFullscreen = () => {
+    if (contentContainerRef.current) {
+      if (contentContainerRef.current.requestFullscreen) {
+        contentContainerRef.current.requestFullscreen();
+      } else if (contentContainerRef.current.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        contentContainerRef.current.webkitRequestFullscreen();
+      }
+      contentContainerRef.current.style.position = "absolute";
+      contentContainerRef.current.style.top = "50%";
+      contentContainerRef.current.style.left = "50%";
+      contentContainerRef.current.style.transform = "translate(-50%, -50%)";
+    }
+  };
   
   return (
     <div className="app">
-      
+        <Header />
+        
         <div className="app-sidebar-container">
           {!showAddNodeBar && (
             <Nav onAddNodeClick={handleAddNodeClick} />
           )}
           {showAddNodeBar && (
-            <AddNodeBar
+            <AddNodeBar className = "add-node-nav" id="add-node-nav"
               onGoBackClick={handleGoBackClick}
               onSubmit={handleCreatePerson}
             />
-          )}          
+          )}      
         </div>
         
-
-        <div className="family-tree-container">
-          <FamilyTree members={members} onMemberClick={handleMemberClick} />
-          {currentMember && <p>Current member: {currentMember.name}</p>}
+        <div className="content-container" ref={contentContainerRef}>
+          <div className="family-tree-container">
+            <FamilyTree members={members} onMemberClick={handleMemberClick} />
+          </div>
+          {currentMember && (
+            <div className="current-member-container">
+              <p>Current member: {currentMember.name}</p>
+            </div>
+          )}
         </div>
       
       <div className="app-footer">
-        <Footer />
+        <Footer 
+          onZoomIn={handleZoomIn}
+          onZoomOut={handleZoomOut}
+          onEnterFullscreen={handleEnterFullscreen}
+        />
       </div>
     </div>
   );
