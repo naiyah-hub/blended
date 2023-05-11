4f75a048bc26cb2de565eafde173593584be58c0
Abdel-Rahman Nasser <96590314+AbdelRahmanNasser20@users.noreply.github.com>
2023-05-11 07:04:40 -0400

    changed family array and fixed addchild impl

--- src/components/TreeComponent/family.js
+++ src/components/TreeComponent/family.js
@@ -1,4 +1,4 @@
-export default [
+const family = [
   {
     id: 1,
     name: "Bob",
@@ -7,7 +7,7 @@ export default [
       name: "Emma",
     },
     dob: "03/03/1892",
-    dod: "02/06/1966",
+    isAlive: false,
     children: [
       {
         id: 3,
@@ -16,23 +16,24 @@ export default [
           id: 4,
           name: "Manish",
           parents: [
-            { id: 5, name: "Samantha", dob: "01/01/1900", dod: "01/01/1980" },
-            { id: 6, name: "Vincent", dob: "01/01/1895", dod: "01/01/1975" },
+            { id: 5, name: "Samantha", dob: "01/01/1900", isAlive: false },
+            { id: 6, name: "Vincent", dob: "01/01/1895", isAlive: false },
           ],
         },
         dob: "15/12/1922",
-        dod: "2002",
+        isAlive: false,
         children: [
           {
             id: 7,
             name: "smita",
             dob: "03/04/1950",
+            isAlive: true,
             children: [
-              { id: 8, name: "Prashant", dob: "03/04/1980" },
-              { id: 9, name: "Mohan", dob: "28/02/1985" },
+              { id: 8, name: "Prashant", dob: "03/04/1980", isAlive: true },
+              { id: 9, name: "Mohan", dob: "28/02/1985", isAlive: true },
             ],
           },
-          { id: 10, name: "pushpa", dob: "18/07/1955" },
+          { id: 10, name: "pushpa", dob: "18/07/1955", isAlive: true },
         ],
       },
       {
@@ -42,36 +43,40 @@ export default [
           id: 12,
           name: "Olivia",
           parents: [
-            { id: 13, name: "George", dob: "01/01/1910", dod: "01/01/1990" },
-            { id: 14, name: "Sophie", dob: "01/01/1915", dod: "01/01/2000" },
+            { id: 13, name: "George", dob: "01/01/1910", isAlive: false },
+            { id: 14, name: "Sophie", dob: "01/01/1915", isAlive: false },
           ],
         },
         dob: "10/02/1927",
+        isAlive: true,
         children: [
-          { id: 15, name: "Frank", dob: "02/12/1945", avatar: "" },
-          { id: 16, name: "Jasmine", dob: "04/03/1947" },
+          { id: 15, name: "Frank", dob: "02/12/1945", avatar: "", isAlive: true },
+          { id: 16, name: "Jasmine", dob: "04/03/1947", isAlive: true },
         ],
       },
     ],
   },
 ];
 
-export const addChild = (members, parentId, child) => {
-  const findAndUpdateParent = (members) => {
-    for (let i = 0; i < members.length; i++) {
-      if (members[i].id === parentId) {
-        members[i].children.push(child);
-        return true;
-      } else if (members[i].children.length > 0) {
-        if (findAndUpdateParent(members[i].children)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  };
 
-  const newMembers = JSON.parse(JSON.stringify(members)); // Create a deep copy of members
-  findAndUpdateParent(newMembers);
-  return newMembers;
-};
+export function addChild(family, parentId, newChild) {
+  return family.map(member => {
+    if (member.id === parentId) {
+      return {
+        ...member,
+        children: [...member.children, newChild]
+      };
+    }
+
+    if (member.children && member.children.length) {
+      return {
+        ...member,
+        children: addChild(member.children, parentId, newChild)
+      };
+    }
+
+    return member;
+  });
+}
+
+export default family;
